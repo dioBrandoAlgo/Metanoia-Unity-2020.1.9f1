ae69e713c5f89c388a8fb4c6eda2a9fc54fb39bc
DiosMussolinos <gabrielvergari@hotmail.com>
2021-04-14 18:47:10 +0100

    Player Movement & Camera Movement

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public float mouseSense = 100f;
+
+    public Transform player;
+
+    public float XRot = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSense;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSense;
+
+        XRot -= mouseY;
+        XRot = Mathf.Clamp(XRot, -80f, 80f);
+
+        transform.localRotation = Quaternion.Euler(XRot, 0f, 0f);
+        player.Rotate(Vector3.up * mouseX);
+
+        
+
+    }
+}
---
+++ Assets/Scripts/ControlAndMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlAndMovement : MonoBehaviour
+{
+
+    //Public
+    public float movementSpeed = 8f;
+    public float rotationSpeed = 3f;
+    //Private
+
+    //Get Components
+    private CharacterController controller;
+    //public Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        controller = GetComponent<CharacterController>();
+        //camera = GetComponent<Camera>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementX = Input.GetAxis("Vertical");
+        float movementZ = Input.GetAxis("Horizontal");
+
+        Vector3 move = transform.forward * movementX + transform.right * movementZ;
+
+        controller.SimpleMove(move * movementSpeed);
+        
+
+    }
+
+
+}
