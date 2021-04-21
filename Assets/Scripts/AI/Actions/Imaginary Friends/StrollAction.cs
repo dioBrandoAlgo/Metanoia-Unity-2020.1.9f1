b2930c5ecbc8132dbe68eab078fd70d81c3cc5c4
Maria <kandushk@gmail.com>
2021-04-21 06:11:36 +0100

    General Organization

---
+++ Assets/Scripts/AI/Actions/Imaginary Friends/StrollAction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Finite State Machine/Action/Imaginary Friends/Stroll")]
+public class StrollAction : Action
+{
+    public override void Act(FSM entity)
+    {
+        throw new System.NotImplementedException();
+    }
+}
